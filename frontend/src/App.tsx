99bb1cb34641b515f8f153b3ed5382de60148cbf
SergioLTorres <sergiotorres0564@gmail.com>
2022-11-19 19:12:44 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- return(
-  <h1>Ola Ola Ola Mundo </h1>
- )
+  return (
+    <>
+      <h1>Ola Ola Ola Mundo </h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
